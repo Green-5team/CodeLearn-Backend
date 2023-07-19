9714978730afe373e25e3256510b8cec8545f347
markdj92 <markdj9205@gmail.com>
2023-07-19 14:01:33 +0900

    Init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,24 @@
-import { Module } from '@nestjs/common';
-import { RoomModule } from './room/room.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { GatewayModule } from "./gateway/gateway.module";
+import { Module } from "@nestjs/common";
+import { RoomModule } from "./room/room.module";
+import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
-import { RoomController } from './room/room.controller';
-import { RoomService } from './room/room.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { User,UserSchema} from './user/schemas/user.schema';
-
+import { UserModule } from "./user/user.module";
+import { UserController } from "./user/user.controller";
+import { UserService } from "./user/user.service";
+import { User, UserSchema } from "./user/schemas/user.schema";
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath : '.env',
+      envFilePath: ".env",
       isGlobal: true,
     }),
-    RoomModule, 
+    GatewayModule,
     MongooseModule.forRoot(process.env.MONGODB_ID),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UserModule,
+    RoomModule,
   ],
-  controllers: [RoomController, UserController],
-  providers: [RoomService, UserService]
+  controllers: [],
+  providers: [],
 })
-
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { ValidationPipe } from "@nestjs/common";
+import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors : true});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  
-   const config = new DocumentBuilder()
-    .setTitle('Code-Learn API')
-    .setDescription('code-learn project from krafton jungle')
-    .setVersion('0.0.1')
+  app.useStaticAssets(join(__dirname, "..", "public")); //javascript,css파일을 서빙해주는 역할, join이라는 매서드는 __dirname현재디렉토리를 가리킴, ..은 현재디렉토리의 상위폴더,public폴더를 가리킴
+  app.setBaseViewsDir(join(__dirname, "..", "views")); //template engine을 어디 폴더에 둘 것인지
+  app.setViewEngine("hbs");
+
+  const config = new DocumentBuilder()
+    .setTitle("Code-Learn API")
+    .setDescription("code-learn project from krafton jungle")
+    .setVersion("0.0.1")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
 }
