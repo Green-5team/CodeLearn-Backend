e46ecfe59497637c9f386bcb0e627ebaaef03657
tocomon <tocomon09@gmail.com>
2023-07-19 16:53:05 +0900

    fix: get userid to createroom

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -3,8 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
 import { Room, RoomSchemas } from './schemas/room.schema'; 
+import { UserModule } from 'src/user/user.module';
+import { User } from 'src/user/schemas/user.schema';
+
 @Module({
   imports: [
+    UserModule,
     MongooseModule.forFeature([{ name: Room.name, schema: RoomSchemas }]),
   ],
   providers: [RoomService],
