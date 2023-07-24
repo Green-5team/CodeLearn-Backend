6009a6502f6c780f61e4f2b8297150b70a978548
markdj92 <129148762+markdj92@users.noreply.github.com>
2023-07-24 20:15:34 +0900

    Update roomanduser.schema.ts
    잘 못 올라간 파일

--- src/room/schemas/roomanduser.schema.ts
+++ src/room/schemas/roomanduser.schema.ts
@@ -6,15 +6,13 @@ import { ApiProperty } from '@nestjs/swagger';
 @Schema()
 export class RoomAndUser extends Document {
 
-  @ApiProperty({ required: true, type: [String] })
   @Prop({ required: true, type: MongooseSchema.Types.ObjectId })
   room_id: MongooseSchema.Types.ObjectId;
 
-  @ApiProperty({ required: true, type: [String] })
+ 
   @Prop({ required: true, type: [String] })
   user_info: string[] ;
 
-  @ApiProperty({ required: true, type: [Boolean], default : false})
   @Prop({ required: true, type: [Boolean], default : false})
   ready_status : boolean[];
 
